174cf16085f972adcf940c851a2fcd0dc246834e
vitalii-22 <smulka69@gmail.com>
2024-01-21 16:18:26 +0200

    Додав бібліотеку і стилі

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,12 +1,15 @@
 import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
 
 const button = document.querySelector('button[data-start]');
 const spanDays = document.querySelector('span[data-days]');
 const spanHours = document.querySelector('span[data-hours]');
 const spanMinutes = document.querySelector('span[data-minutes]');
 const spanSeconds = document.querySelector('span[data-seconds]');
+const input = document.querySelector('#datetime-picker');
 
 let userSelectedDate = 0;
+button.disabled = true;
 
 const options = {
   enableTime: true,
@@ -16,11 +19,28 @@ const options = {
   onClose(selectedDates) {
     const date = new Date();
     if (date > selectedDates[0]) {
-      button.disabled = true;
       iziToast.show({
-        title: 'Hey',
-        message: 'What would you like to add?',
+        title: 'Error',
+        titleColor: '#FFF',
+        titleSize: '16px',
+        titleLineHeight: '24px',
+
+        message: 'Please choose a date in the future',
+        color: '#EF4040',
+        iconUrl: './img/javascript.svg#icon-bi_x-octagon',
+        iconColor: '#FFF',
+        position: 'topRight',
+        messageSize: '16px',
+        messageColor: '#FFF',
+        messageLineHeight: '24px',
       });
+
+      // iziToast.warning({
+      //   title: 'Caution',
+      //   message: 'You forgot important data',
+      // });
+
+      button.disabled = true;
     } else {
       userSelectedDate = selectedDates[0];
       button.disabled = false;
@@ -31,7 +51,9 @@ const options = {
 flatpickr('#datetime-picker[type = text]', options);
 
 function start() {
-  setInterval(() => {
+  const intervalId = setInterval(() => {
+    input.disabled = true;
+    button.disabled = true;
     const startTime = new Date();
     const diff = userSelectedDate - startTime;
     const time = convertMs(diff);
@@ -42,8 +64,11 @@ function start() {
       spanMinutes.textContent = addLeadingZero(minutes);
       spanSeconds.textContent = addLeadingZero(seconds);
     }
-
     onTick(time);
+
+    if (diff < 1000) {
+      clearInterval(intervalId);
+    }
   }, 1000);
 }
 
@@ -72,6 +97,7 @@ function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
+console.log(convertMs(0));
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
